a6f9a63f18dc3b7b3a035731bfb39f7b9ef8370b
Arnah <github@arnah.ca>
2023-03-04 19:19:29 -0700

    Change the default pluginhub to a github repository

--- src/main/java/ca/arnah/runelite/RuneLiteHijackProperties.java
+++ src/main/java/ca/arnah/runelite/RuneLiteHijackProperties.java
@@ -14,7 +14,7 @@ public class RuneLiteHijackProperties{
 	private static final String PLUGINHUB_BASE = "runelitehijack.pluginhub.url";
 	
 	public static List<HttpUrl> getPluginHubBase(){
-		return Arrays.stream(System.getProperty(PLUGINHUB_BASE, "https://files.arnah.ca/runelite").split(","))
+		return Arrays.stream(System.getProperty(PLUGINHUB_BASE, "https://raw.githubusercontent.com/Arnuh/RuneLiteHijack-PluginHub/master/").split(","))
 			.map(HttpUrl::parse)
 			.collect(Collectors.toList());
 	}
